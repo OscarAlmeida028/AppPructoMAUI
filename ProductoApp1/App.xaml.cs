1cdfba09403c361ce388254f69a832eb2227534a
oscar.almeida <oscaralmeida028@gmail.com>
2023-11-26 15:07:10 -0500

    Agregar archivos de proyecto.

---
+++ ProductoApp1/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+namespace ProductoApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new ProductoPage());
+            //MainPage = new ProductoPage();
+        }
+    }
+}
